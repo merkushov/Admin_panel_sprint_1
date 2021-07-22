d9967c1b63ff5d1a72da9d8da59344e0590367f2
Victor Merkushov <merkushov.victor@gmail.com>
2021-07-22 23:14:44 +0300

    Подправил стиль кода

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -8,24 +8,28 @@ from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor, execute_batch
 import uuid
 
+
 @dataclass(frozen=True)
 class Person():
     __slots__ = ("id", "name")
     id: str
     name: str
 
-@dataclass(frozen=True)
-class Writer(Person): pass
 
 @dataclass(frozen=True)
-class Actor(Person): pass
+class Writer(Person):
+    pass
+
+
+@dataclass(frozen=True)
+class Actor(Person):
+    pass
+
 
 @dataclass(frozen=False)
 class Movie:
-    # __slots__ = ("id", "title", "imdb_rating", "genres", "description", "directors", "writers", "actors")
     imdb_identifier: str
     title: str
-    # uuid: str
     imdb_rating: float = field(default=0.0)
     genres: list = field(default_factory=list)
     description: str = field(default='')
@@ -43,7 +47,8 @@ class PostgresSaver():
 
         execute_batch(
             cur,
-            "INSERT INTO content.persons (full_name) VALUES (%s) ON CONFLICT (full_name) DO NOTHING",
+            "INSERT INTO content.persons (full_name) VALUES (%s) "
+            "ON CONFLICT (full_name) DO NOTHING",
             [(person_name,) for person_name in persons]
         )
 
@@ -57,7 +62,8 @@ class PostgresSaver():
 
         execute_batch(
             cur,
-            "INSERT INTO content.genres (name) VALUES (%s) ON CONFLICT (name) DO NOTHING",
+            "INSERT INTO content.genres (name) VALUES (%s) "
+            "ON CONFLICT (name) DO NOTHING",
             [(genre_name,) for genre_name in genres]
         )
 
@@ -69,18 +75,35 @@ class PostgresSaver():
     def save_movies_data(self, movies: list[Movie]):
         cur = self.pg_connection.cursor()
 
-        cur.execute("SELECT id FROM content.movie_types WHERE name=%s", ('фильм',))
+        cur.execute(
+            "SELECT id FROM content.movie_types WHERE name=%s", ('фильм',)
+        )
         movie_type = cur.fetchone()
 
         if not movie_type:
-            raise psycopg2.DataError("There is no movie_type with name 'фильм'")
+            raise psycopg2.DataError(
+                "There is no movie_type with name 'фильм'"
+            )
 
         # Создаём Фильмы
         execute_batch(
             cur,
-            "INSERT INTO content.movies (title, type_id, imdb_identifier, imdb_rating, description) "
-            "VALUES (%s, %s, %s, %s, %s) ON CONFLICT (imdb_identifier) DO NOTHING",
-            [ ( m.title, movie_type[0], m.imdb_identifier, m.imdb_rating, m.description ) for m in movies ],
+            "INSERT INTO content.movies "
+            "   ("
+            "       title, type_id, imdb_identifier, "
+            "       imdb_rating, description"
+            "   )"
+            "   VALUES (%s, %s, %s, %s, %s) "
+            "   ON CONFLICT (imdb_identifier) DO NOTHING",
+            [
+                (
+                    m.title,
+                    movie_type[0],
+                    m.imdb_identifier,
+                    m.imdb_rating,
+                    m.description
+                ) for m in movies
+            ],
         )
 
         self.pg_connection.commit()
@@ -95,10 +118,16 @@ class PostgresSaver():
             cur,
             "INSERT INTO content.genre_movie (movie_id, genre_id)"
             "       SELECT m.id, g.id"
-            "           FROM movies m LEFT JOIN (SELECT id FROM genres WHERE name=%s) g ON 1=1"
+            "           FROM movies m LEFT JOIN "
+            "               (SELECT id FROM genres WHERE name=%s) g ON 1=1"
             "           WHERE m.imdb_identifier IN %s"
             "   ON CONFLICT (movie_id, genre_id) DO NOTHING",
-            [ ( genre, tuple(genre_movie[genre]) ) for genre in genre_movie.keys() ]
+            [
+                (
+                    genre,
+                    tuple(genre_movie[genre])
+                ) for genre in genre_movie.keys()
+            ]
         )
 
         self.pg_connection.commit()
@@ -106,35 +135,72 @@ class PostgresSaver():
         # Связываем Фильмы и Персоны (актёров, режисёров, сценаристов)
         movie_person_role = {}
         for movie in movies:
-            movie_person_role.setdefault(movie.imdb_identifier, {}).setdefault('actors', ['stumb'])
-            movie_person_role.setdefault(movie.imdb_identifier, {}).setdefault('directors', ['stumb'])
-            movie_person_role.setdefault(movie.imdb_identifier, {}).setdefault('writers', ['stumb'])
+            movie_person_role.setdefault(
+                movie.imdb_identifier, {}
+            ).setdefault('actors', ['stumb'])
+            movie_person_role.setdefault(
+                movie.imdb_identifier, {}
+            ).setdefault('directors', ['stumb'])
+            movie_person_role.setdefault(
+                movie.imdb_identifier, {}
+            ).setdefault('writers', ['stumb'])
             for actor in movie.actors:
-                movie_person_role[movie.imdb_identifier]['actors'].append(actor.name)
+                movie_person_role[movie.imdb_identifier]['actors'].append(
+                    actor.name
+                )
             for director in movie.directors:
-                movie_person_role[movie.imdb_identifier]['directors'].append(director)
+                movie_person_role[movie.imdb_identifier]['directors'].append(
+                    director
+                )
             for writer in movie.writers:
-                movie_person_role[movie.imdb_identifier]['writers'].append(writer.name)
+                movie_person_role[movie.imdb_identifier]['writers'].append(
+                    writer.name
+                )
 
         execute_batch(
             cur,
-            "INSERT INTO content.person_person_role_movie (movie_id, person_id, person_role_id)"
+            "INSERT INTO content.person_person_role_movie"
+            "   (movie_id, person_id, person_role_id)"
             "       SELECT m.id, p.id, pr.id"
             "           FROM content.movies m"
-            "               INNER JOIN (SELECT id FROM content.persons WHERE full_name IN %s) p ON 1=1"
-            "               INNER JOIN (SELECT id FROM content.person_roles WHERE name = 'актёр') pr ON 1=1"
+            "               INNER JOIN ("
+            "                   SELECT id"
+            "                       FROM content.persons"
+            "                       WHERE full_name IN %s"
+            "               ) p ON 1=1"
+            "               INNER JOIN ("
+            "                   SELECT id"
+            "                       FROM content.person_roles"
+            "                       WHERE name = 'актёр'"
+            "               ) pr ON 1=1"
             "           WHERE m.imdb_identifier=%s"
             "       UNION ALL"
             "       SELECT m.id, p.id, pr.id"
             "           FROM content.movies m"
-            "               INNER JOIN (SELECT id FROM content.persons WHERE full_name IN %s) p ON 1=1"
-            "               INNER JOIN (SELECT id FROM content.person_roles WHERE name = 'сценарист') pr ON 1=1"
+            "               INNER JOIN ("
+            "                   SELECT id"
+            "                       FROM content.persons"
+            "                       WHERE full_name IN %s"
+            "               ) p ON 1=1"
+            "               INNER JOIN ("
+            "                   SELECT id"
+            "                       FROM content.person_roles"
+            "                       WHERE name = 'сценарист'"
+            "               ) pr ON 1=1"
             "           WHERE m.imdb_identifier=%s"
             "       UNION ALL"
             "       SELECT m.id, p.id, pr.id"
             "           FROM content.movies m"
-            "               INNER JOIN (SELECT id FROM content.persons WHERE full_name IN %s) p ON 1=1"
-            "               INNER JOIN (SELECT id FROM content.person_roles WHERE name = 'режисёр') pr ON 1=1"
+            "               INNER JOIN ("
+            "                   SELECT id"
+            "                       FROM content.persons"
+            "                       WHERE full_name IN %s"
+            "                   ) p ON 1=1"
+            "               INNER JOIN ("
+            "                   SELECT id"
+            "                       FROM content.person_roles"
+            "                       WHERE name = 'режисёр'"
+            "                   ) pr ON 1=1"
             "           WHERE m.imdb_identifier=%s"
             "   ON CONFLICT (movie_id, person_id, person_role_id) DO NOTHING",
             [
@@ -142,13 +208,15 @@ class PostgresSaver():
                     tuple(movie_person_role[imdb_key]['actors']),  imdb_key,
                     tuple(movie_person_role[imdb_key]['writers']),  imdb_key,
                     tuple(movie_person_role[imdb_key]['directors']),  imdb_key,
-                ) for imdb_key in movie_person_role ]
+                ) for imdb_key in movie_person_role
+            ]
         )
 
         self.pg_connection.commit()
         cur.close()
 
         return True
+
 
 class SQLiteLoader():
     def __init__(self, conn: sqlite3.Connection) -> None:
@@ -171,7 +239,9 @@ class SQLiteLoader():
         in_placeholders = ','.join(['?'] * len(unique_data))
 
         rows = self.sqlite_connection.execute(
-            f"SELECT id, name FROM writers WHERE id IN ({in_placeholders}) AND name != 'N/A'",
+            "SELECT id, name"
+            "   FROM writers"
+            "   WHERE id IN %s AND name != 'N/A'",
             list(unique_data.keys())
         ).fetchall()
 
@@ -183,13 +253,10 @@ class SQLiteLoader():
 
     def _get_movie_actors(self, movie_id: str):
         rows = self.sqlite_connection.execute(
-            ''.join(
-                (
-                    "SELECT a.id, a.name ",
-                    "FROM actors a LEFT JOIN movie_actors ms ON a.id=ms.actor_id ",
-                    "WHERE ms.movie_id = ? AND a.name != 'N/A'",
-                )
-            ),
+            "SELECT a.id, a.name"
+            "   FROM actors a"
+            "       LEFT JOIN movie_actors ms ON a.id=ms.actor_id"
+            "   WHERE ms.movie_id = %s AND a.name != 'N/A'",
             (movie_id,)
         ).fetchall()
 
@@ -207,20 +274,35 @@ class SQLiteLoader():
 
     def load_movies(self, start: int, limit: int) -> list:
         sqlite_movies = self._get_movies(start, limit)
-        
+
         movies = []
         for row in sqlite_movies:
+
+            imdb_rating = 0.0
+            if not self._check_na(row["imdb_rating"]):
+                imdb_rating = float(row["imdb_rating"])
+
+            description = ''
+            if not self._check_na(row["plot"]):
+                description = row["plot"]
+
+            directors = []
+            if not self._check_na(row["director"]):
+                directors = [x.strip() for x in row['director'].split(',')]
+
             movies.append(
                 Movie(
-                    # uuid = str(uuid.uuid4()),
-                    imdb_identifier = row["id"],
-                    imdb_rating = 0.0 if self._check_na(row["imdb_rating"]) else float(row["imdb_rating"]),
-                    genres = row["genre"].replace(' ', '').split(','),
-                    title = row["title"],
-                    description = '' if self._check_na(row["plot"]) else row["plot"],
-                    directors = [] if self._check_na(row["director"]) else [x.strip() for x in row['director'].split(',')],
-                    writers = self._get_movie_writers(row["writer"], row["writers"]),
-                    actors = self._get_movie_actors(row["id"])
+                    imdb_identifier=row["id"],
+                    imdb_rating=imdb_rating,
+                    genres=row["genre"].replace(' ', '').split(','),
+                    title=row["title"],
+                    description=description,
+                    directors=directors,
+                    writers=self._get_movie_writers(
+                        row["writer"],
+                        row["writers"]
+                    ),
+                    actors=self._get_movie_actors(row["id"])
                 )
             )
 
@@ -229,17 +311,26 @@ class SQLiteLoader():
     def load_persons(self, start: int, limit: int) -> list:
         persons = []
 
-        rows = self.sqlite_connection.execute(f"SELECT name FROM actors LIMIT {start}, {limit}").fetchall()
+        rows = self.sqlite_connection.execute(
+            "SELECT name FROM actors LIMIT %s, %s",
+            (start, limit)
+        ).fetchall()
         for row in rows:
             persons.append(row["name"])
 
-        rows = self.sqlite_connection.execute(f"SELECT director FROM movies LIMIT {start}, {limit}").fetchall()
+        rows = self.sqlite_connection.execute(
+            "SELECT director FROM movies LIMIT %s, %s",
+            (start, limit)
+        ).fetchall()
         for row in rows:
             if not self._check_na(row["director"]):
                 for x in row['director'].split(','):
                     persons.append(x.strip())
 
-        rows = self.sqlite_connection.execute(f"SELECT name FROM writers LIMIT {start}, {limit}").fetchall()
+        rows = self.sqlite_connection.execute(
+            "SELECT name FROM writers LIMIT %s, %s",
+            (start, limit)
+        ).fetchall()
         for row in rows:
             persons.append(row["name"])
 
@@ -248,12 +339,16 @@ class SQLiteLoader():
     def load_genres(self, start: int, limit: int) -> list:
         genres = {}
 
-        rows = self.sqlite_connection.execute(f"SELECT genre FROM movies LIMIT {start}, {limit}").fetchall()
+        rows = self.sqlite_connection.execute(
+            "SELECT genre FROM movies LIMIT %s, %s",
+            (start, limit)
+        ).fetchall()
         for row in rows:
             for genre in row["genre"].replace(' ', '').split(','):
                 genres.setdefault(genre, None)
 
         return list(genres.keys())
+
 
 class EnhancedJSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -302,7 +397,7 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
 
 
 if __name__ == '__main__':
-    pg_dsl = {
+    pg_dsn = {
         'dbname': 'movie_catalog',
         'user': 'postgres',
         # 'password': 1234,
@@ -312,6 +407,6 @@ if __name__ == '__main__':
         'options': '-c search_path=content',
     }
 
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**pg_dsl, cursor_factory=DictCursor) as pg_conn:
+    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**pg_dsn, cursor_factory=DictCursor) as pg_conn:
         sqlite_conn.row_factory = sqlite3.Row
         load_from_sqlite(sqlite_conn, pg_conn)
