16685bf73bc93163a4ccd13c5ffefb5ff08b01b6
Victor Merkushov <merkushov.victor@gmail.com>
2021-08-11 11:19:40 +0300

    Небольшие исправления

--- movies_admin/entrypoint.sh
+++ movies_admin/entrypoint.sh
@@ -8,6 +8,9 @@ python manage.py migrate --noinput
 echo "Creating super user..."
 python manage.py createsuperuser --noinput
 
+echo "Compiling localization files..."
+python manage.py compilemessages
+
 echo "Gathering static files in a single directory..."
 python manage.py collectstatic --noinput
 
